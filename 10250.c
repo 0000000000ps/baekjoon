5981711e929f192b898cb5fac16fe453d6ca46b2
Sunmin Lee <sulee@c3r1s3.42seoul.kr>
2020-11-08 17:12:32 +0900

    201108

--- 10250.c
+++ 10250.c
@@ -4,19 +4,33 @@ int		main(void)
 {
 	int i = 0;
 	int T;
-	int H, W, N;
-	int Y, X, num;
+	int H[99];
+	int W[99];
+	int N[9801];
+	int Y, X;
 
 	scanf("%d", &T);
 	while (i < T)
 	{
-		scanf("%d %d %d", &H, &W, &N);
+		scanf("%d %d %d", &H[i], &W[i], &N[i]);
 		rewind(stdin);
 		i++;
 	}
-	Y = N % H * 100;
-	X = N / H + 1;
-	num = Y + X;
-	printf("%d\n", num);
+	i = 0;
+	while (i < T && N[i] <= H[i] * W[i])
+	{
+		if (N[i] % H[i] == 0)
+			Y = N[i] / H[i] * 100;
+		else if (N[i] <= H[i])
+			Y = N[i] * 100;
+		else
+			Y = N[i] % H[i] * 100;
+		if (N[i] % H[i] == 0)
+			X = H[i];
+		else
+			X = N[i] / H[i] + 1;
+		printf("%d\n", Y + X);
+		i++;
+	}
 	return 0;
 }
